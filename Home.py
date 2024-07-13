52b1fe06dc887c0d6b0ce2e8ef823a2b539fa1b5
jumpingchu <jumpingchu@gmail.com>
2024-07-13 20:27:26 +0800

    Feat: Add Multiselect Filter Components & view_count Modification

--- Home.py
+++ Home.py
@@ -1,8 +1,14 @@
 import streamlit as st
-from utils import read_bq_table
+from utils import read_bq_table, multiselect_yyyymm, multiselect_score
 
 st.set_page_config(page_title="動畫瘋 - 近期動畫評分", layout="wide")
 st.title("動畫瘋 - 近期動畫評分")
+
+
+def format_to_ten_thousand(value):
+    value = int(value)
+    return f"{value / 10000:.2f}"
+
 
 gcp_secrets = st.secrets["gcp"]
 df = read_bq_table(
@@ -11,6 +17,9 @@ df = read_bq_table(
     gcp_secrets["table_name"],
     gcp_secrets,
 )
+
+# 處理觀看人數的格式
+df["view_count"] = df["view_count"].apply(format_to_ten_thousand)
 
 df = df.rename(
     columns={
@@ -50,11 +59,16 @@ column_config = {
     ),
 }
 
-yyyymm_options = sorted(list(df["上映時間"].unique()), reverse=True)
-default_yyyymm = yyyymm_options[:2]
-selected_yyyymm = st.multiselect("選擇上映時間", options=yyyymm_options, default=default_yyyymm)
+col_yyyymm_multiselect, col_score_multiselect, _ = st.columns(3)
+with col_yyyymm_multiselect:
+    selected_yyyymm = multiselect_yyyymm(df)
+with col_score_multiselect:
+    selected_score = multiselect_score(df)
+
 if selected_yyyymm:
     df = df[df["上映時間"].isin(selected_yyyymm)]
+if selected_score:
+    df = df[df["評分"].isin(selected_score)]
 
 st.dataframe(
     df,
